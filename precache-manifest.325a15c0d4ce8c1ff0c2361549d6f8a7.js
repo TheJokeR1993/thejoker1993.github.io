54c7f935355d68b555be4d6a1b8b335ebd62eec8
TheJokeR1993 <thejokermafiaclub@gmail.com>
2022-06-02 13:27:40 +0300

    Updates

---
+++ precache-manifest.325a15c0d4ce8c1ff0c2361549d6f8a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c0b41de158f394f7e09eeedb581b8ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b802beaf2319169b8783",
+    "url": "/static/css/main.2b75f2c7.chunk.css"
+  },
+  {
+    "revision": "2ecac21e3c7e3fd7fdf3",
+    "url": "/static/js/2.9aca678d.chunk.js"
+  },
+  {
+    "revision": "3c9d7f8ac5cde9d15f5e6bbb17a202fa",
+    "url": "/static/js/2.9aca678d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b802beaf2319169b8783",
+    "url": "/static/js/main.367cf672.chunk.js"
+  },
+  {
+    "revision": "f64bbedbc980241ccee0",
+    "url": "/static/js/runtime-main.18399cad.js"
+  },
+  {
+    "revision": "51c934afb61ee321ae0d9f05a4b432e9",
+    "url": "/static/media/getFetch.51c934af.cjs"
+  }
+]);
